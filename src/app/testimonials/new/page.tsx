34e8c4220ecbe760822fdeea59dde8fc401a7106
Julien Lisita <julien.lisita@gmail.com>
2025-10-22 18:48:16 +0200

    chore(cleanup): remove unused components (NewsCard, NewTestimonial, etc.)

--- src/app/testimonials/new/page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/temoignages/new/page.tsx
-
-import NewTestimonial from '@/components/pages/NewTestimonial';
-
-export const metadata = { title: 'Laisser un témoignage' };
-
-export default function NewTestimonialPage() {
-  return <NewTestimonial />;
-}
