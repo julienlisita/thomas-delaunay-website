b8c78dcbe627ad1dc24bf07b94aef3e7ed8c5d71
Julien Lisita <julien.lisita@gmail.com>
2025-10-23 15:56:45 +0200

    feat(home): add hero section with titles, CTA, and background image

--- src/components/section/Hero.tsx
+++ src/components/section/Hero.tsx
@@ -2,23 +2,48 @@
 
 'use client';
 
+import Image from 'next/image';
 import Button from '@/components/ui/Button';
 import PageTitle from '@/components/ui/PageTitle';
+// import './Hero.css';
 
 export default function Hero() {
   return (
     <section
-      className="w-full  flex flex-col justify-center  text-[#3B2F2F] px-4 text-center"
+      className="hero w-full  flex flex-col justify-center  text-[#3B2F2F] px-4 text-center"
       style={{ minHeight: 'calc(100vh - var(--header-height))' }}
     >
-      <div className="max-w-4xl mx-auto space-y-6">
-        <PageTitle> Mettez en valeur votre activité avec un site professionnel</PageTitle>
-        <p className="text-lg sm:text-xl text-[#3B2F2F]/80">
-          Un design clair, responsive et rapide à mettre en place.
+      {/* Image de fond */}
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{ backgroundImage: "url('/images/home/hero.avif') " }}
+      />
+      {/* Overlay noir avec opacité */}
+      <div className="absolute inset-0 bg-black/60" />
+
+      {/* Logo */}
+      <div className="max-w-4xl mx-auto space-y-6 z-10">
+        <div className="flex justify-center">
+          <Image
+            src="/images/logo.png"
+            alt="Company Care & Services logo"
+            width={300}
+            height={200}
+          />
+        </div>
+        <PageTitle color="#FFFFFF"> Le sport à votre rythme</PageTitle>
+        <p className="text-sm font-bold sm:text-lg text-white/80">
+          Retrouvez énergie et confiance grâce à un accompagnement personnalisé, bienveillant et
+          durable.
         </p>
-        <Button variant="primary" href="/contact">
-          Demander un devis
-        </Button>
+        <div className="flex flex-col sm:flex-row justify-center  items-center gap-4">
+          <Button href="/contact" variant="primary">
+            Me contacter
+          </Button>
+          <Button href="/coaching" variant="secondary">
+            Programmes
+          </Button>
+        </div>
       </div>
     </section>
   );
