d7914308a3361485ca43f5e5e5e4b169c846e378
Julien Lisita <julien.lisita@gmail.com>
2025-11-15 23:12:53 +0100

    fix(tailwind): include utils directory in content scan to generate alignment classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/app/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/utils/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
       colors: {
