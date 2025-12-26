efcc96911d660321be0c6a63b3304cf6b909a790
Julien Lisita <julien.lisita@gmail.com>
2025-12-26 12:11:58 +0100

    feat(layout): reset scroll position on route change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 // src/app/layout.tsx
 
+import ScrollToTopOnRouteChange from '@/components/layout/ScrollToTopOnRouteChange';
 import './globals.css';
 import { metadata } from './metadata';
 export { metadata };
@@ -7,6 +8,7 @@ export { metadata };
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
+      <ScrollToTopOnRouteChange />
       <body className="min-h-screen flex flex-col antialiased">{children}</body>
     </html>
   );
---
+++ src/components/layout/ScrollToTopOnRouteChange.tsx
@@ -0,0 +1,16 @@
+// src/components/layout/ScrollToTopOnRouteChange.tsx
+
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { useEffect } from 'react';
+
+export default function ScrollToTopOnRouteChange() {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+  }, [pathname]);
+
+  return null;
+}
